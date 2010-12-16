830ef39ca05c6b5e2aed378405096fed4eb91f22
Yakazoo <yakazoo@yakazoo-desktop.(none)>
2010-12-16 19:21:08 +0100

    Finished!!!

--- ex04/main2.cpp
+++ ex04/main2.cpp
@@ -10,5 +10,53 @@
 //
 //=================================================================================================
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void tokenize(vector<string>& sub_str, string str, const char seperator){ 	
+     size_t n = str.find(seperator); 	
+     if(n != string::npos){ 		
+          sub_str.push_back(str.substr(0,n)); 		
+          //tokenize the remaining elements of the line 		
+          tokenize(sub_str,str.substr(n+1,str.length()),seperator); 	
+     } 	
+     else{ 		
+             //End of line reached 		
+             sub_str.push_back(str); 	
+     } 
+}
+
+template <typename T>
+struct PrintVec{
+	inline void operator()(const T val){cout << val << " ";}
+};
 
 
+int main(int argc, char* argv[]){
+	if(argc != 3){
+		cout << "Error: not enough arguments" << endl;
+		cout << "Example: ./main2 file1.in file2.out" << endl;
+	}
+	
+	string str_words;
+    vector<string> vec_words;
+    fstream filestr;
+  
+    filestr.open(argv[1],fstream::in);	  
+    getline(filestr,str_words);
+    filestr.close();
+    tokenize(vec_words, str_words, ' ');
+    //Print Vecgtor
+	for_each(vec_words.begin(),vec_words.end(),PrintVec<string>());
+	  cout << endl;
+    sort(vec_words.begin(),vec_words.end());
+    //Print Vecgtor
+	for_each(vec_words.begin(),vec_words.end(),PrintVec<string>());
+	  cout << endl;
+    
+	return 0;	
+}
