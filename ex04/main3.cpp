079c62a3cbd5f7149079b12afdb3d66a16985d43
Yakazoo <yakazoo@yakazoo-desktop.(none)>
2010-12-19 22:11:00 +0100

    edited and finished

--- ex04/main3.cpp
+++ ex04/main3.cpp
@@ -10,5 +10,41 @@
 //
 //=================================================================================================
 
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+using namespace std;
+
+template <typename T>
+struct PrintVec{
+	inline void operator()(const T val){cout << val << " ";}
+};
+
+unsigned int fib(unsigned int n){
+   if(n == 0 || n == 1) return 1;
+   else return fib(n-2) + fib(n-1);
+}
+
+int main(void){
+ 
+  vector<string> str_vec;
+
+  for(unsigned int i = 0; i < 10;++i){
+    stringstream sstream;
+    sstream << fib(i);
+    str_vec.push_back(sstream.str());
+  }
+  sort(str_vec.begin(),str_vec.end());
+  
+  //Print Vector
+    for_each(str_vec.begin(),str_vec.end(),PrintVec<string>());
+	  cout << endl;
+    
+    return 0;
+  
+  
+}
 
 
