63e46ad697599c386ac443e9a9fd5322fdb10a53
Yakazoo <yakazoo@yakazoo-desktop.(none)>
2010-12-19 22:11:43 +0100

    edited and finished

--- ex04/main9.cpp
+++ ex04/main9.cpp
@@ -13,5 +13,39 @@
 //
 //=================================================================================================
 
+#include <iostream>
+#include <set>
+
+struct Comp
+{
+	bool operator() (const unsigned int& first, const unsigned int& sec) const
+		{ return first < sec; }	// the given ordering seem to be a simple arithmetic ordering...
+};
+
+bool firstDigIsOne (unsigned int i)
+{
+	while (i > 9) i /= 10;	// only regard the first digit
+	return (1 == i);
+}
+
+int main(int argc, char **argv)
+{
+	std::set<unsigned int, Comp> sint;
+
+	for (unsigned int i = 15; i > 0; --i)
+		sint.insert(i * i + i * 3 + 2);
+
+	for (std::set<unsigned int>::const_iterator it = sint.begin(); it != sint.end(); ++it)
+		std::cout << *it << "\t";
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	for (std::set<unsigned int>::iterator it = sint.begin(); it != sint.end(); ++it)
+		if (firstDigIsOne(*it)) sint.erase(it);
+
+	for (std::set<unsigned int>::const_iterator it = sint.begin(); it != sint.end(); ++it)
+		std::cout << *it << "\t";
+	std::cout << std::endl;
+}
 
 
