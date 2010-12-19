4b2c7426e27cc471ff8ddc65ed51da8645df8149
Yakazoo <yakazoo@yakazoo-desktop.(none)>
2010-12-19 22:11:33 +0100

    edited and finished

--- ex04/main8.cpp
+++ ex04/main8.cpp
@@ -9,5 +9,28 @@
 //
 //=================================================================================================
 
+#include <iostream>
+#include <set>
 
+bool isEven (int i)
+	{ return (0 == (i % 2)); }
 
+int main(int argc, char **argv)
+{
+	std::set<int> sint;
+
+	for (int i = 15; i > 0; --i)
+		sint.insert(i * 3 + 2);
+
+	for (std::set<int>::const_iterator it = sint.begin(); it != sint.end(); ++it)
+		std::cout << *it << "\t";
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	for (std::set<int>::iterator it = sint.begin(); it != sint.end(); ++it)
+		if (isEven(*it)) sint.erase(it);
+
+	for (std::set<int>::const_iterator it = sint.begin(); it != sint.end(); ++it)
+		std::cout << *it << "\t";
+	std::cout << std::endl;
+}
